69ac15f649d24afd858ceae346e50a378eb5cea4
nobuh <nobu.hatano@gmail.com>
2021-05-15 22:49:49 +0900

    func names changed to start with i

--- examples/8dots/toyshader.go
+++ examples/8dots/toyshader.go
@@ -27,15 +27,15 @@ var gl_FragColor = ivec4{ 0, 0, 0, 255 }
 var gl_FragCoord = ivec2{ 0, 0 }
 
 // integer radius 100 per 10 degree cache
-var sin100[36]int16
-var cos100[36]int16
+var isin[36]int16
+var icos[36]int16
 
 func distanceFromO(v ivec2) int16 {
 	return v.x * v.x + v.y * v.y
 }
 
 
-func length(a ivec2, b ivec2) int16 {
+func ilength(a ivec2, b ivec2) int16 {
 	return distanceFromO(ivec2{a.x - b.x, a.y - b.y})
 }
 
@@ -51,9 +51,9 @@ func pseudoShader() {
 
 	c := int16(0);
 	for i := int16(0); i < 360; i += 360 / ncircle {
-		x := orbit_radius * sin100[i / 10] / 100 + o.x
-		y := orbit_radius * cos100[i / 10] / 100 + o.y
-		l := length(gl_FragCoord, ivec2{ x, y })
+		x := orbit_radius * isin[i / 10] / 100 + o.x
+		y := orbit_radius * icos[i / 10] / 100 + o.y
+		l := ilength(gl_FragCoord, ivec2{ x, y })
 		c += 255 * dot_radius / l
 	}
 
@@ -68,8 +68,8 @@ func pseudoShader() {
 func main() {
 	// init sin cos cache
 	for i := int16(0); i < 36; i++ {
-		sin100[i] = int16(100 * math.Sin(2.0 * 3.14 * float64(i) / 36))
-		cos100[i] = int16(100 * math.Cos(2.0 * 3.14 * float64(i) / 36))
+		isin[i] = int16(100 * math.Sin(2.0 * 3.14 * float64(i) / 36))
+		icos[i] = int16(100 * math.Cos(2.0 * 3.14 * float64(i) / 36))
 	}
 
 	display.Configure()
