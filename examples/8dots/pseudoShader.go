ec1fdc46f99363adaf3d9fb325705ade12ea87cf
nobuh <nobu.hatano@gmail.com>
2021-05-16 13:16:19 +0900

    separate main and pseudoShader

---
+++ examples/8dots/pseudoShader.go
@@ -0,0 +1,30 @@
+// Write Your Shader in pseudoShader()
+//
+// resolution_x = 240
+// resolution_y = 160
+//
+// all variable type should be int16
+// ivec2 has x,y 
+// ivec4 has R,G,B,A
+//
+// R,G,B,A = 0..255, 0..255, 0..255, 0..255
+
+func pseudoShader() {
+
+	const ncircle = int16(8)
+	const orbit_radius  = resolution_y / 3
+	const dot_radius = int16(30)
+
+	o := ivec2{ resolution_x / 2, resolution_y / 2 }
+
+	c := int16(0);
+	for i := int16(0); i < 360; i += 360 / ncircle {
+		x := orbit_radius * sin100(i) / 100 + o.x
+		y := orbit_radius * cos100(i) / 100 + o.y
+		l := ilength(gl_FragCoord, ivec2{ x, y })
+		c += 255 * dot_radius / l
+	}
+
+	if c > 255 { c = 255 }
+	gl_FragColor = ivec4{c, c, c, 255}
+}
---
+++ toyshader.go
@@ -0,0 +1,81 @@
+package main
+
+import (
+	"image/color"
+	"machine"
+	"math"
+)
+
+var display = machine.Display
+
+const resolution_x = int16(240)
+const resolution_y = int16(160)
+
+type ivec2 struct {
+	x int16
+	y int16
+}
+
+type ivec4 struct {
+	r int16
+	g int16
+	b int16
+	a int16
+}
+
+var gl_FragColor = ivec4{ 0, 0, 0, 255 }
+var gl_FragCoord = ivec2{ 0, 0 }
+
+// radius 100, sin/cos each 10 degrees
+var sin100Cache[36]int16
+var cos100Cache[36]int16
+
+
+func sin100(degree int16) int16 {
+	return sin100Cache[degree / 10]
+}
+
+
+func cos100(degree int16) int16 {
+	return cos100Cache[degree / 10]
+}
+
+
+func distanceFromO(v ivec2) int16 {
+	l := int32(v.x * v.x + v.y * v.y)
+	if l > 32767 { l = 32767 }
+	return int16(l)
+}
+
+
+func ilength(a ivec2, b ivec2) int16 {
+	return distanceFromO(ivec2{a.x - b.x, a.y - b.y})
+}
+
+
+func main() {
+	// init sin cos cache
+	for i := int16(0); i < 36; i++ {
+		sin100Cache[i] = int16(100 * math.Sin(2.0 * 3.14 * float64(i) / 36))
+		cos100Cache[i] = int16(100 * math.Cos(2.0 * 3.14 * float64(i) / 36))
+	}
+
+	display.Configure()
+
+	for y := int16(0); y < resolution_y; y++ {
+		for x:= int16(0); x < resolution_x; x++ {
+
+			gl_FragCoord = ivec2{ x, y }
+
+			pseudoShader()
+
+			r := uint8(gl_FragColor.r * gl_FragColor.a / 255)
+			g := uint8(gl_FragColor.g * gl_FragColor.a / 255)
+			b := uint8(gl_FragColor.b * gl_FragColor.a / 255)
+
+			display.SetPixel(x, y, color.RGBA{ r, g, b, 0})  // alpha has no effect
+		}
+	}
+
+	display.Display()
+}
