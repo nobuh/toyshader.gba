a7f67faaedecc55027849faad6a2bf2cfb70d21f
nobuh <nobu.hatano@gmail.com>
2021-05-17 20:05:27 +0900

    gradient

---
+++ examples/gradient/pseudoShader.go
@@ -0,0 +1,6 @@
+func pseudoShader() {
+
+	// if 255 * 255, the value over int16, so divide 255 to 51 * 5 
+	st := vec2{ 51 * gl_FragCoord.x / resolution_x * 5, 51 * gl_FragCoord.y / resolution_y * 5 }
+	gl_FragColor = vec4{ st.x, st.y, 0, 255}
+}
