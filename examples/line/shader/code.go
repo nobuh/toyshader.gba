f7ecadd85616e4ae543167c25d42953c474b232f
nobuh <nobu.hatano@gmail.com>
2021-05-24 21:23:18 +0900

    monotone slope

--- examples/line/shader/code.go
+++ examples/line/shader/code.go
@@ -1,11 +1,20 @@
 package shader
 
+func plot(st vec2) int16 {
+	return smoothstep(5, 0, abs(st.y - st.x))
+}
+
+
 func shader() {
 	st := vec2{ normalize * gl_FragCoord.x / u_resolution.x, normalize * gl_FragCoord.y / u_resolution.y }
 
-	y := smoothstep(5, 0, abs(st.y - st.x))
+	y := st.x
+	color := vec3{ y, y, y }
 
-	color := vec3{ 0, cdepth * y / normalize, 0 }
+	pct := plot(st)
+	color.r = (normalize - pct) * color.r
+	color.g = (normalize - pct) * color.g + pct * normalize
+	color.b = (normalize - pct) * color.b
 
 	gl_FragColor = color
 }
